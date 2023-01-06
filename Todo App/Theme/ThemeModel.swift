cf64981eaf6baee1eb3ca4bf53cb4d2bcdde0b9c
Eduardo Ceron <ing.ceron@192.168.5.31>
2023-01-06 06:29:14 -0600

    Initial Commit

---
+++ Todo App/Theme/ThemeModel.swift
@@ -0,0 +1,15 @@
+//
+//  Todo App
+//
+//  Created by Eduardo Ceron on 24/03/22.
+//
+
+import SwiftUI
+
+// MARK: - THEME MODEL
+
+struct Theme: Identifiable {
+  let id: Int
+  let themeName: String
+  let themeColor: Color
+}
